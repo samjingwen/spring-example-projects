4511c00f6307574b0879e10baaf07011e64208b3
samjingwen <samjingwen@u.nus.edu>
2022-09-14 23:01:01 +0800

    always be coding

---
+++ java-native-k8s-config/configuration/src/main/java/io/samjingwen/configuration/ConfigurationApplication.java
@@ -0,0 +1,36 @@
+package io.samjingwen.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
+import org.springframework.boot.cloud.CloudPlatform;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class ConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationApplication.class, args);
+	}
+
+	private final Environment environment;
+
+	@EventListener({ApplicationReadyEvent.class, RefreshScopeRefreshedEvent.class})
+	public void begin() {
+		System.out.println("the message is " + this.environment.getProperty("message"));
+	}
+
+	@Bean
+	@ConditionalOnCloudPlatform(CloudPlatform.KUBERNETES)
+	ApplicationListener<ApplicationReadyEvent> applicationListenerReadyEvent() {
+		return args -> 	System.out.println("the application is running on k8s!");
+	}
+
+}
---
+++ java-native-k8s-config/configuration/src/test/java/io/samjingwen/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.samjingwen.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
